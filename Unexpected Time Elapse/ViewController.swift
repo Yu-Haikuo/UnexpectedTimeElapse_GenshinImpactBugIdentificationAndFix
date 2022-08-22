4247b94170ad01f08f260cfaa3db8e05ae9a45c9
Yu Haikuo <e0376983@u.nus.edu>
2022-08-22 23:22:00 +0800

    Initial Commit

---
+++ Unexpected Time Elapse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unexpected Time Elapse
+//
+//  Created by HAIKUO YU on 22/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
