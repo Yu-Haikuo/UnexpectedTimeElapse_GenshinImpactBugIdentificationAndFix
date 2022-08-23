5faf7d35ab23a7492e9210e01ccaf8af2a26e0ff
Yu Haikuo <e0376983@u.nus.edu>
2022-08-23 16:25:18 +0800

    Linked storyboard with its view controller and programmtically set missing anchors.

--- Unexpected Time Elapse/ViewController.swift
+++ Unexpected Time Elapse/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var beforeFixProgressView: UIProgressView!
+    @IBOutlet weak var afterFixProgressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NSLayoutConstraint.activate([
+            beforeFixProgressView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+            beforeFixProgressView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60),
+            
+            afterFixProgressView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 80),
+            afterFixProgressView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -60)
+        ])
     }
-
-
+    
+    
+    
 }
 
